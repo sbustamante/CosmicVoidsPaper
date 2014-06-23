849d513df1557e428ee453825f36d7baa9973aa1
sbustamante <macsebas33@gmail.com>
2014-06-23 05:37:48 +0000

    Boundary removals over density watershed

--- bench/codes_figures/cosmicweb_density_field.py
+++ bench/codes_figures/cosmicweb_density_field.py
@@ -3,7 +3,7 @@
 #This code perform a graphic scheme of the visual impresion for a defined cutting off of Bolshoi
 #simulation, using the density field it is performed a classification scheme of voids based upon
 #cuts of the density.
-#Usage cosmicweb_density_field.py <catalogue, BDM or FOF> <show(0) or save(1)>
+#Usage cosmicweb_density_field.py <catalogue, BDM or FOF> <show(0) or save(1)> <format (png, pdf)>
 #
 #by: Sebastian Bustamante
 
@@ -24,8 +24,8 @@ catalog = sys.argv[1]
 
 #Void catalogue
 void_scheme = "DLG"
-#Void parameter (For FAG and DLG schemes, it is the number of iterations for the median filtering)
-lambda_void = 1.0
+#Void parameters ( Nth-order median filtering,  Boolean for boundary removals )
+config = "01"
 #Smooth parameter
 smooth = '_s1'
 #Coordinate to cut (1 -- X, 2 -- Y, 3 -- Z)
@@ -65,8 +65,8 @@ delta_filename = '%s%sTweb/%d/Delta%s'%(foldglobal,simulation,N_sec,smooth)
 #Loading general catalog of halos
 GH = np.loadtxt('%s%sC_GH_%s.dat'%(foldglobal,simulation,catalog))
 #Catalogue of voids
-voids = CutFieldZ( "%s/%s/%s/%d/voids%s/voids_%1.2f/void_index.dat"%\
-(foldglobal, simulation, "Tweb", N_sec, void_scheme, lambda_void ), Cut, 'plain', Coor = axe )
+voids = CutFieldZ( "%s/%s/%s/%d/voids%s/voids_%s/void_index.dat"%\
+(foldglobal, simulation, "Tweb", N_sec, void_scheme, config ), Cut, 'plain', Coor = axe )
 #Loading Fields
 delta = CutFieldZ( delta_filename, Cut, 32, Coor = axe )
 
@@ -117,6 +117,9 @@ plt.ylim( (0,Box_L) )
 
 
 if sys.argv[2] == '1':
-    plt.savefig( '%scosmicweb_density.pdf'%(figures_fold) )
+    if sys.argv[3] == 'png':
+	plt.savefig( '%scosmicweb_density.png'%(figures_fold) )
+    else:
+	plt.savefig( '%scosmicweb_density.pdf'%(figures_fold) )
 else:
     plt.show()
