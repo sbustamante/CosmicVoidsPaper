3eca78beabf833fc507279f90ec53e2428517afc
sbustamante <macsebas33@gmail.com>
2015-05-19 15:34:51 -0500

    Histograms of FA for each environment in a different new figure. Caption

--- bench/codes_figures/FA_environment_histogram.py
+++ bench/codes_figures/FA_environment_histogram.py
@@ -37,31 +37,33 @@ for web in webs:
     #Loading files with quartiles
     histogram = np.loadtxt( '%sFA_Environment_%s.dat'%(data_figures_fold,web) )
 
+    hist = np.zeros( (50,4) )
+    for env in xrange(4):
+	  hist[:,env], bins = np.histogram( histogram[:,0], bins = 50, weights = histogram[:,env+1], density=True )
+	  if env == 0:
+	      hist[bins[:-1]>0.95,env]=0
     for env in xrange(4):
 	  plt.subplot(2,1,i_web+1)
-	  hist, bins = np.histogram( histogram[:,0], bins = 50, weights = histogram[:,env+1], density=True )
-	  if env == 0:
-	      hist[bins[:-1]>0.95]=0
-	  plt.plot( bins[:-1], hist, ls = linestyles[env], lw = linewidths[env],\
+	  plt.plot( bins[:-1], hist[:,env]/(hist[:,0]+hist[:,1]+hist[:,2]+hist[:,3]), ls = linestyles[env], lw = linewidths[env],\
 	  label = labels[env], color=colors[env] )
 	
     i_web += 1
 
 plt.subplot(2,1,1)
-plt.legend( loc="upper left", fancybox=True, shadow=True, fontsize=12, ncol=2 )
+plt.legend( loc="center left", fancybox=True, shadow=True, fontsize=10, ncol=2 )
 plt.grid()
 plt.xlim((0,1))
-plt.ylim((0,25))
-plt.ylabel("Probability Density $P(FA)$", fontsize = 11)
-plt.text( 0.02, 6, "Tweb" )
+plt.ylim((0,1))
+plt.ylabel("$(dV_i/dFA)/(dV/dFA)$", fontsize = 11)
+plt.text( 0.88, 0.9, "Tweb" )
 
 plt.subplot(2,1,2)
 plt.grid()
 plt.xlim((0,1))
-plt.ylim((0,25))
+plt.ylim((0,1))
 plt.xlabel("FA")
-plt.ylabel("Probability Density $P(FA)$", fontsize = 11)
-plt.text( 0.02, 6, "Vweb" )
+plt.ylabel("$(dV_i/dFA)/(dV/dFA)$", fontsize = 11)
+plt.text( 0.88, 0.9, "Vweb" )
 
 if sys.argv[1] == '1':
     plt.savefig( '%sFA_environment.pdf'%(figures_fold) )
